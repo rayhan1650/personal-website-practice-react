720a40e00ccf83759cec7c5c8d5fc22224d741f5
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-06-24 20:23:07 +0600

    services section added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Services from "./components/Services/Services";
 import TopBanner from "./components/TopBanner/TopBanner";
 
 function App() {
   return (
     <div>
       <TopBanner />
+      <Services />
     </div>
   );
 }
