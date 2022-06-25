2b274cef296865332d26298f596f4afe7ad6c5e4
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-06-25 12:05:48 +0600

    nav scroll css added

--- src/components/TopNavigation/TopNavigation.js
+++ src/components/TopNavigation/TopNavigation.js
@@ -1,12 +1,25 @@
-import React from "react";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const TopNavigation = () => {
+  const [navBarTitle, setNavBarTitle] = useState("navTitle");
+
+  const changeNavbar = () => {
+    if (window.scrollY > 100) {
+      setNavBarTitle("navTitleScroll");
+    } else {
+      setNavBarTitle("navTitle");
+    }
+  };
+  window.addEventListener("scroll", changeNavbar);
+
   return (
     <>
       <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container fluid>
-          <Navbar.Brand href="#home">Rayhan</Navbar.Brand>
+          <Navbar.Brand href="#home" className={navBarTitle}>
+            Rayhan
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
