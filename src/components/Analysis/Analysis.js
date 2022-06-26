40dfe686243efc494f02c7cc5c9d1409bd077591
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-06-26 10:51:34 +0600

    barchart added in analysis

--- src/components/Analysis/Analysis.js
+++ src/components/Analysis/Analysis.js
@@ -1,7 +1,42 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { ResponsiveContainer, BarChart, Bar, XAxis, Tooltip } from "recharts";
 
 const Analysis = () => {
+  const data = [
+    {
+      name: "Java",
+      Projects: 400,
+    },
+    {
+      name: "Kotlin",
+      Projects: 300,
+    },
+    {
+      name: "SQL",
+      Projects: 200,
+    },
+    {
+      name: "Bootstrap",
+      Projects: 278,
+    },
+    {
+      name: "Jquery",
+      Projects: 189,
+    },
+    {
+      name: "React",
+      Projects: 239,
+    },
+    {
+      name: "PHP",
+      Projects: 349,
+    },
+    {
+      name: "Angular",
+      Projects: 309,
+    },
+  ];
   return (
     <>
       <Container>
@@ -9,7 +44,15 @@ const Analysis = () => {
           Technology Used
         </h1>
         <Row>
-          <Col lg={6} md={12} sm={6}></Col>
+          <Col lg={6} md={12} sm={6}>
+            <ResponsiveContainer width="100%" height="100%">
+              <BarChart width={150} height={40} data={data}>
+                <XAxis dataKey="name" stroke="#0073e6" />
+                <Tooltip />
+                <Bar dataKey="Projects" fill="#0073e6" />
+              </BarChart>
+            </ResponsiveContainer>
+          </Col>
           <Col lg={6} md={12} sm={6}>
             <p className="analysisText">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
