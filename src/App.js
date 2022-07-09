96039ac0b60b9a4be2104e4509accd7f498bf41f
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-07-09 21:19:57 +0600

    aboutPage created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
+import AboutDescription from "./components/AboutDescription/AboutDescription";
 import PageTop from "./components/PageTop/PageTop";
 import TopNavigation from "./components/TopNavigation/TopNavigation";
+import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div>
-      <TopNavigation />
-      <PageTop />
+      <AboutPage />
     </div>
   );
 }
---
+++ src/components/AboutDescription/AboutDescription.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+const AboutDescription = () => {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <h2 className="mt-5">Who I Am</h2>
+            <hr />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi
+              laborum magni sequi mollitia, veritatis velit quae deleniti, autem
+              a repellat, excepturi cupiditate in veniam rem cum commodi animi
+              deserunt enim harum? Eius, adipisci nesciunt? Fuga consequatur
+              culpa cum, accusamus consectetur non omnis, accusantium explicabo
+              adipisci perspiciatis expedita eius deleniti laborum?
+            </p>
+            <h2 className="mt-5">My Mission</h2>
+            <hr />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi
+              laborum magni sequi mollitia, veritatis velit quae deleniti, autem
+              a repellat, excepturi cupiditate in veniam rem cum commodi animi
+              deserunt enim harum? Eius, adipisci nesciunt? Fuga consequatur
+              culpa cum, accusamus consectetur non omnis, accusantium explicabo
+              adipisci perspiciatis expedita eius deleniti laborum?
+            </p>
+            <h2 className="mt-5">My Vision</h2>
+            <hr />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi
+              laborum magni sequi mollitia, veritatis velit quae deleniti, autem
+              a repellat, excepturi cupiditate in veniam rem cum commodi animi
+              deserunt enim harum? Eius, adipisci nesciunt? Fuga consequatur
+              culpa cum, accusamus consectetur non omnis, accusantium explicabo
+              adipisci perspiciatis expedita eius deleniti laborum?
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default AboutDescription;
