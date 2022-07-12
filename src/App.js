df0a6cd3318d7c7a1065e7819492f546e6606e24
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-07-12 10:49:51 +0600

    contact form created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import AboutDescription from "./components/AboutDescription/AboutDescription";
+import ContactSection from "./components/ContactSecton/ContactSection";
 import PageTop from "./components/PageTop/PageTop";
 import TopNavigation from "./components/TopNavigation/TopNavigation";
 import AboutPage from "./pages/AboutPage";
@@ -10,7 +11,7 @@ import PortfolioPage from "./pages/PortfolioPage";
 function App() {
   return (
     <div>
-      <PortfolioPage />
+      <ContactSection />
     </div>
   );
 }
---
+++ src/components/ContactSecton/ContactSection.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { MdEmail, MdCall } from "react-icons/md";
+
+const ContactSection = () => {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col lg={6} md={6} sm={12}>
+            <h2>Quick Connect</h2>
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" rows="3" />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+          <Col lg={6} md={6} sm={12}>
+            <h2>Discuss Now</h2>
+            <p className="mb-0 text-secondary">
+              Ward 06, Monohardi Pauroshava, Narsingdi
+            </p>
+            <small className="text-secondary">
+              <MdEmail className="me-1" />
+              rayhannurmd@gmail.com
+            </small>
+            <br />
+            <small className="text-secondary">
+              <MdCall className="me-1" />
+              01521211324
+            </small>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default ContactSection;
