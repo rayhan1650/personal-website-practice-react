3067a6b9726fe6249f16754cf71d98cc774dacb5
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-07-23 15:49:45 +0600

    axios appUrl added

---
+++ src/restApi/AppUrl.js
@@ -0,0 +1,26 @@
+const AppUrl = () => {
+  const baseUrl = "http://localhost:8000";
+
+  const chartData = `${baseUrl}/chartData`;
+  const clientReview = `${baseUrl}/clientReview`;
+  const contactSend = `${baseUrl}/contactSend`;
+
+  const courseHome = `${baseUrl}/courseHome`;
+  const courseAll = `${baseUrl}/courseAll`;
+  const courseDetails = `${baseUrl}/courseDetails`;
+
+  const footer = `${baseUrl}/footer`;
+  const information = `${baseUrl}/information`;
+  const services = `${baseUrl}/services`;
+
+  const projectsHome = `${baseUrl}/projectsHome`;
+  const projectsAll = `${baseUrl}/projectsAll`;
+  const projectsDetails = `${baseUrl}/projectsDetails`;
+
+  const videoHome = `${baseUrl}/videoHome`;
+  const totalProjectClient = `${baseUrl}/totalProjectClient`;
+  const techDesc = `${baseUrl}/techDesc`;
+  const homeTopTitle = `${baseUrl}/homeTopTitle`;
+};
+
+export default AppUrl;
