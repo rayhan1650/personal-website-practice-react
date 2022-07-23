2cbecf0135d32fb98d584285186bbac6b4ac6152
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-07-23 15:29:12 +0600

    axios applied successfully

--- src/components/TopBanner/TopBanner.js
+++ src/components/TopBanner/TopBanner.js
@@ -1,7 +1,19 @@
+import axios from "axios";
 import React, { Fragment } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 
 const TopBanner = () => {
+  axios
+    .get("http://localhost:8000/homeTopTitle")
+    .then(function (response) {
+      // handle success
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+
   return (
     <Fragment>
       <Container fluid className="topFixedBanner p-0">
